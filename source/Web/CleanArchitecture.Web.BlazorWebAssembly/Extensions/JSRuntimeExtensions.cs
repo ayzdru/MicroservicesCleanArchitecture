dc281af83b2f8234409defbeb04f9af2aa70ba91
Ayaz Duru <ayzdru@outlook.com>
2020-07-03 17:19:00 +0300

    Sepet sistemi ürün silme eklendi.

---
+++ source/Web/CleanArchitecture.Web.BlazorWebAssembly/Extensions/JSRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace CleanArchitecture.Web.BlazorWebAssembly.Extensions
+{
+    public static class JSRuntimeExtensions
+    {
+        public static ValueTask<bool> Confirm(this IJSRuntime jsRuntime, string message)
+        {
+            return jsRuntime.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
